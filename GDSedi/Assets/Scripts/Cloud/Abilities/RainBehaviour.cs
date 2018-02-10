de5d27e72b30f61eb978ed62f8268531b4521d3e
Iain Cattermole <iain_c@live.co.uk>
2018-02-10 13:12:06 +0000

    one cloudy boi

---
+++ GDSedi/Assets/Scripts/Cloud/Abilities/RainBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainBehaviour : MonoBehaviour, CloudAbility {
+
+	private int charges;
+	private int maxCharges;
+
+	public int GetCharges() {
+		return charges;
+	}
+
+	void Awake() {
+
+	}
+
+	public bool PerformAbility() {
+		if (charges <= 0) {
+			return false;
+		} else {
+			charges--;
+			//TODO
+		}
+	}
+
+
+}
---
+++ GDSedi/Assets/Scripts/Cloud/CloudMovementBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMovementBehaviour : MonoBehaviour {
+
+	public float SPEED = 1f;
+
+	private Rigidbody2D rb;
+
+	void Awake () {
+		rb = this.gameObject.GetComponent<Rigidbody2D>();
+	}
+
+	void Update () {
+		float xSpeed = 0;
+		float ySpeed = 0;
+
+		if (Input.GetKey(KeyCode.W)) {
+			ySpeed = SPEED;
+		} else if (Input.GetKey(KeyCode.S)) {
+			ySpeed= -SPEED;
+		}
+
+		if (Input.GetKey(KeyCode.A)) {
+			xSpeed = -SPEED;
+		} else if (Input.GetKey(KeyCode.D)) {
+			xSpeed = SPEED;
+		}
+
+		rb.velocity = new Vector3(xSpeed, ySpeed, 0);
+	}
+}
