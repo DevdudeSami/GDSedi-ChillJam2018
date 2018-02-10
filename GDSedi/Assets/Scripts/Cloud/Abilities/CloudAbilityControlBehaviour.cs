7c0047ffab389081c149dc847f87f77c0cb1237d
Iain Cattermole <iain_c@live.co.uk>
2018-02-10 18:20:32 +0000

    workin now

--- GDSedi/Assets/Scripts/Cloud/Abilities/CloudAbilityControlBehaviour.cs
+++ GDSedi/Assets/Scripts/Cloud/Abilities/CloudAbilityControlBehaviour.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CloudAbilityControlBehaviour : MonoBehaviour {
 
+	public GameObject selectedAbilityUI;
+
 	private int selected;
 	private UseAbilityBehaviour[] abilites;
 
