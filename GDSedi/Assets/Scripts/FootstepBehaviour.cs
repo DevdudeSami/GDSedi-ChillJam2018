2f0bac32416c578e9f5cd94b6a01d2e95e8f3e53
Youssef Moawad <youssef_programming@icloud.com>
2018-02-10 13:27:31 +0000

    Added player footsteps prefab and script.

---
+++ GDSedi/Assets/Scripts/FootstepBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start() {
+		yield return new WaitForSeconds(0.75f);
+		destroy();
+	}
+
+	private void destroy() {
+		Destroy(gameObject);
+	}
+
+}
--- GDSedi/Assets/Scripts/Player/PlayerMovementBehaviour.cs
+++ GDSedi/Assets/Scripts/Player/PlayerMovementBehaviour.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class PlayerMovementBehaviour : MonoBehaviour {
 
+	public GameObject footstepPrefab;
+
 	public float speed = 2.0f;
 
 	private Vector3 currentSpeed = new Vector3(0, 0, 0);
-
 	private Rigidbody2D rb;
+
+	private int footstepTimer = 0;
+	private const int footstepInterval = 12;
 
 	void Start() {
 		rb = GetComponent<Rigidbody2D>();
@@ -32,5 +36,17 @@ public class PlayerMovementBehaviour : MonoBehaviour {
 		}
 
 		rb.velocity = currentSpeed;
+
+		updateFootstep();
+	}
+
+	private void updateFootstep() {
+		footstepTimer++;
+
+		if(footstepTimer == footstepInterval) {
+			footstepTimer = 0;
+
+			Instantiate(footstepPrefab, this.transform.position, Quaternion.identity);
+		}
 	}
 }
